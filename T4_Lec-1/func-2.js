7c778cfa0cb4af69ccfdd48a7356e4b61be0a382
Parisha-15 <parishamalvania315@gmail.com>
2025-12-22 13:23:55 +0530

    Add files via upload

---
+++ T4_Lec-1/func-2.js
@@ -0,0 +1,11 @@
+<html>
+    <script>
+function f()
+{
+var a=10    
+console.log(a)
+}
+f()
+console.log(a)
+</script>
+</html>
---
+++ T4_Lec-1/p.js
@@ -0,0 +1,3 @@
+   document.writeln("Hello <br>")
+    document.write("Students")
+    console.log("I am an console for error msg")
