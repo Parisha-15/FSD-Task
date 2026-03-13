7eb78f1833ea8993675e5ab33ca9e1167febd9be
Parisha-15 <parishamalvania315@gmail.com>
2026-03-13 10:50:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var obj={"name":"abc",
+    "age":19,
+    "isPass":true,
+    "address":{"city":"Ahmedabad","zip":"380015"},
+    "subject":["FSD","FSCP","TOC"],
+    "isalloted":null
+}
+console.log(obj.age)
+console.log(obj.address)
+console.log(obj.address.zip)
+console.log(obj.subject[1])
