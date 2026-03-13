39633e2d642e5494ef754a1473af4bb3cfc493a9
Parisha-15 <parishamalvania315@gmail.com>
2026-03-13 10:53:52 +0530

    Add files via upload

---
+++ B3_FSD/a.js
@@ -0,0 +1,12 @@
+var fs= require('fs')
+// fs.writeFileSync("hello.txt","good morning")
+// fs.appendFileSync("hello.txt","How are You ?")
+// fs.appendFileSync("hi.txt","How are You ?")
+// var data=fs.readFileSync("hello.txt")
+// console.log(data) // Buffer data in hexcode 
+// console.log(data.toString())
+// var data1=fs.readFileSync("hello.txt","UTF-8")
+// console.log(data1)
+// fs.renameSync("hi.txt","Hey.txt")
+// fs.unlinkSync("Hey.txt")
+
